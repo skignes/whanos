de7703dd0f820ea82ec1903701a210bb207499e3
Skignes <116216779+skignes@users.noreply.github.com>
2025-10-28 16:55:01 +0100

    feat(link-project): add the link project job

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -5,3 +5,36 @@ folder("Whanos base images") {
 folder("Projects") {
     description("Available projects.")
 }
+
+job("link-project") {
+    parameters {
+        stringParam("GIT_URL", null, 'Git repository url')
+        stringParam("DISPLAY_NAME", null, "Display name for the job")
+        // TODO: Add private ssh key
+    }
+    steps {
+        dsl {
+            text('''
+job("Projects/$DISPLAY_NAME") {
+    scm {
+        git {
+            remote {
+                name("origin")
+                url("$GIT_URL")
+            }
+        }
+    }
+    triggers {
+        scm("* * * * *")
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        shell("echo \\"deploy\\"")
+    }
+}
+            ''')
+        }
+    }
+}
