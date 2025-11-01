911b10cef43e1e73c832b3d02ebc638487a03422
Skignes <116216779+skignes@users.noreply.github.com>
2025-11-01 18:56:49 +0100

    feat(jenkins): add the full thing without deploy

--- jenkins/job_dsl.groovy
+++ jenkins/job_dsl.groovy
@@ -1,7 +1,118 @@
 folder("Whanos base images") {
-    description("Whanos base images.")
+  description("Whanos base images.")
 }
 
 folder("Projects") {
-    description("Available projects.")
+  description("Available projects.")
+}
+
+job('/link-project') {
+    parameters{
+        stringParam('GIT_URL', null, 'Git URL')
+        stringParam('DISPLAY_NAME', null, 'Display Name')
+        credentialsParam('CREDENTIAL') {
+            type('com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey')
+            description('SSH key to clone repository')
+        }
+    }
+    steps {
+        dsl('''
+job("/Projects/$DISPLAY_NAME") {
+    scm {
+        git {
+            remote {
+                name('origin')
+                url("$GIT_URL")
+                credentials("$CREDENTIAL")
+            }
+        }
+    }
+    triggers {
+        scm('* * * * *')
+    }
+    wrappers {
+        preBuildCleanup()
+    }
+    steps {
+        shell('/opt/jenkins/deploy.sh')
+    }
+}'''.stripIndent())
+    }
+}
+
+def languages = [
+  [id: "c", name: "whanos-c"],
+  [id: "java", name: "whanos-java"],
+  [id: "javascript", name: "whanos-javascript"],
+  [id: "python", name: "whanos-python"],
+  [id: "befunge", name: "whanos-befunge"]
+]
+
+languages.each { lang ->
+        job("Whanos base images/${lang.name}") {
+                description("Build the base image for ${lang.id} language")
+                steps {
+                        shell("""
+set -e pipefail
+echo "Building base image ${lang.name}"
+
+if [ ! -f /opt/jenkins/images/${lang.id}/Dockerfile.base ]; then
+  echo "Dockerfile.base for ${lang.id} is missing"
+  exit 1
+fi
+
+docker build -t ${lang.name}:latest - < /opt/jenkins/images/${lang.id}/Dockerfile.base
+docker tag ${lang.name}:latest 127.0.0.1:5000/${lang.name}:latest
+docker push 127.0.0.1:5000/${lang.name}:latest
+""")
+                }
+                wrappers { timestamps() }
+        }
+}
+
+pipelineJob('Whanos base images/Build all base images') {
+    description('Trigger all Whanos base images builds.')
+
+    definition {
+        cps {
+            sandbox(false)
+            script('''\
+    import jenkins.model.Jenkins
+
+    def imgs = [ 'whanos-befunge', 'whanos-c', 'whanos-java', 'whanos-javascript', 'whanos-python' ]
+
+    def jobs = [:]
+
+    for (int i = 0; i < imgs.size(); ++i) {
+        def img = imgs[i]
+        def jobFullName = "Whanos base images/${img}"
+        jobs[img] = {
+            def instance = Jenkins.instance.getItemByFullName(jobFullName)
+
+            if (instance == null) {
+                error("Job ${jobFullName} not found")
+            }
+
+            def build = instance.scheduleBuild2(0)
+
+            if (build == null) {
+                error("Failed to schedule ${jobFullName}")
+            }
+
+            def finished_build = build.get()
+            if (finished_build == null) {
+                error("Failed to get build for ${jobFullName}")
+            }
+
+            def result = finished_build.getResult()
+            if (result == null || result.toString() != 'SUCCESS') {
+                error("${jobFullName} finished with status: ${result}")
+            }
+        }
+    }
+
+    parallel jobs
+''')
+        }
+    }
 }
---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,108 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT="${4:-.}"
+NO_BUILD=false
+NO_PUSH=false
+NO_DEPLOY=true
+
+usage(){
+    cat <<EOF
+Usage: $0 [--no-build] [--no-push] [--no-deploy] [path]
+Options:
+    --no-build    Skip image build
+    --no-push     Skip pushing the image
+    --no-deploy   Skip Kubernetes deployment
+    -h, --help    Show this help and exit
+EOF
+}
+
+ROOT="${ROOT:-.}"
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --no-build) NO_BUILD=true; shift ;;
+    --no-push) NO_PUSH=true; shift ;;
+    --no-deploy) NO_DEPLOY=true; shift ;;
+    -h|--help) usage; exit 0 ;;
+    *) ROOT="$1"; shift ;;
+  esac
+done
+
+ROOT=$(realpath "$ROOT")
+
+log(){
+  printf "%s\n" "$*" >&2
+}
+
+if [ ! -d "$ROOT" ]; then
+  log "Error: path '$ROOT' not a directory"
+  exit 1
+fi
+
+detect_language(){
+  local dir="$1"
+  if [ -f "$dir/Makefile" ]; then echo c; return; fi
+  if [ -f "$dir/app/pom.xml" ]; then echo java; return; fi
+  if [ -f "$dir/package.json" ]; then echo javascript; return; fi
+  if [ -f "$dir/requirements.txt" ]; then echo python; return; fi
+  if [ -f "$dir/app/main.bf" ]; then echo befunge; return; fi
+  echo unknown
+}
+
+LANGUAGE=$(detect_language "$ROOT")
+if [ "$LANGUAGE" = "unknown" ]; then
+  log "Error: Failed to determine language"
+  exit 1
+fi
+
+log "Detected language: $LANGUAGE"
+
+REPO_ROOT_DIR="/opt/jenkins/"
+APP_NAME=$(basename "$ROOT")
+IMAGE="127.0.0.1:5000/${APP_NAME}:latest"
+
+build_image(){
+  local dir="$1" image="$2"
+
+  if [ -f "$dir/Dockerfile" ]; then
+    log "Info: Building from provided Dockerfile"
+    docker build -t "$image" -f "$dir/Dockerfile" "$dir"
+    return
+  fi
+
+  local d_standalone="$REPO_ROOT_DIR/images/${LANGUAGE}/Dockerfile.standalone"
+
+  if [ -f "$d_standalone" ]; then
+    log "Info: Buildingn using Whanos image Dockerfile: $d_standalone"
+    docker build -t "$image" -f "$d_standalone" "$dir"
+    return
+  fi
+
+  log "Error: no Dockerfile found"
+  return 1
+}
+
+push_image(){
+  local image="$1"
+
+  log "Info: Pushing image $image"
+  docker push "$image"
+}
+
+# Build -> Push -> Deploy
+if [ "$NO_BUILD" = false ]; then
+  log "Building image: $IMAGE"
+  build_image "$ROOT" "$IMAGE"
+fi
+
+if [ "$NO_PUSH" = false ]; then
+  push_image "$IMAGE"
+fi
+
+if [ "$NO_DEPLOY" = false ]; then
+  deploy "$IMAGE" "$ROOT"
+fi
+
+log "Info: Done."
+exit 0
